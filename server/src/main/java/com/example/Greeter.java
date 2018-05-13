14f8abd5efaeecd706e6440baa2e16493c159a85
Ricardo Viera <rvscript@gmail.com>
2018-05-12 22:17:38 -0400

    Jenkins Checkstyle Warnings corrected on all saved files

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,10 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+ /*
+  Comment for greet method per Jenkins
+ */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
